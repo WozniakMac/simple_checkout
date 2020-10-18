2690e678d83b891145c1bdda51a45e9f27e95428
WozniakMac <maciekwo@gmail.com>
2020-10-18 18:28:21 +0100

    Remove fake product db

--- app/checkout/product.rb
+++ app/checkout/product.rb
@@ -1,28 +1,9 @@
 class Product
-  @@product_db = []
-  attr_accessor :code, :name, :price
+  attr_reader :code, :name, :price
 
   def initialize(code, name, price)
     @code = code
     @name = name
     @price = Money.new(price, Config::CURRENCY)
-  end
-
-  def self.create(code, name, price)
-    product = new(code, name, price)
-    @@product_db << product
-    product
-  end
-
-  def self.find(code)
-    @@product_db.find { |product| product.code == code }
-  end
-
-  def self.count
-    @@product_db.count
-  end
-
-  def self.delete_all
-    @@product_db = []
   end
 end
